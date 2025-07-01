59bd71897da183d837c0014c3ddf8378d27ee2d1
wdario.c7@gmail.com <wdario.c7@gmail.com>
2025-07-01 06:20:45 -0500

    new change 2

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from "readline-sync";
 export function sayHiToUser()
 {
     console.log('¡Bienvenido a Brain Games!');
-    const name = readlineSync.question("Cual es tu nombre?: ");
+    const name = readlineSync.question("¿Cual es tu nombre?: ");
     console.log(`¡Hola, ${name}!`);
 }
 
