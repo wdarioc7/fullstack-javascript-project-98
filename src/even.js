22fffb8eaa86a91b8406508b5816ddf0ebb3e2cf
wdario.c7@gmail.com <wdario.c7@gmail.com>
2025-07-01 06:39:56 -0500

    nuevos cambios

--- src/even.js
+++ src/even.js
@@ -6,28 +6,29 @@ export function esPar() {
  
   while (true) {
     const numero = Math.floor(Math.random() * 100) + 1;
-    console.log(`El número aleatorio generado es: ${numero}`);
+    console.log(`Pregunta: ${numero}`);
 
-    const respuesta = readlineSync.question('¿Es este número par? Responde "yes" si es par, "no" si es impar: ').toLowerCase();
+    const respuesta = readlineSync.question('Responde "yes" si el número es par, de lo contrario responde "no". ').toLowerCase();
 
     const esPar = numero % 2 === 0;
     const esCorrecto = (esPar && respuesta === 'yes') || (!esPar && respuesta === 'no');
 
     if (esCorrecto) {
-      console.log('¡Respuesta correcta!');
+      console.log(`Tu respuesta: ${respuesta }`);
       streak++;
     } else {
-      console.log('Respuesta incorrecta. Fin del juego.');
-      console.log(`Tu racha fue de ${streak} aciertos.`);
+      console.log(`'yes' es una respuesta incorrecta ;(. La respuesta correcta era '${esPar ? 'yes' : 'no'}'.`);
+      console.log(`¡Intentémoslo de nuevo!`);
+      //console.log(`Tu racha fue de ${streak} aciertos.`);
       break;
     }
 
     if (streak >= 3) {
-      console.log(`¡Felicidades! Has alcanzado una racha de ${streak} aciertos.`);
+      console.log(`¡Felicidades!, Bill!`);
       break;
     }
 
-    console.log(`Tu racha actual es de ${streak} aciertos.`);
+    //console.log(`Tu racha actual es de ${streak} aciertos.`);
 
     const continuar = readlineSync.question('¿Quieres seguir jugando? (yes/no): ').toLowerCase();
     if (continuar !== 'yes') {
